178c60e3758a42ace9e861c78ece928482cee8f2
zach <zach@drivenirc.com>
2009-01-23 23:59:23 -0500

    added adminserv.c

---
+++ src/adminserv.c
@@ -0,0 +1,10 @@
+/* AdminServ (Source)
+ *
+ * (C) 2009 Xanadu IRC Services Team
+ *
+ * Based on the original code of Anope by the Anope Team.
+ * Based on the original code of Epona by Lara.
+ * Based on the original code of Services by Andy Church.
+ *
+ * $Id: ns_addnick.c 1.0 01-17-2009 $
+ */
