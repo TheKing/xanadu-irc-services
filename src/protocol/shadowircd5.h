c107f11a64cd509549b12484fbfd07174a19d47f
theking <theking@theking-desktop.(none)>
2009-01-25 12:28:41 -0500

    Added ShadowIRCd5 Functions file and Edited changes file.

---
+++ src/protocol/shadowircd5.h
@@ -0,0 +1,11 @@
+/* Protocol : ShadowIRCd 5 Function Reference File 
+ *
+ * (C) 2009 Xanadu IRC Services Team
+ *
+ * Based on the original code of Anope by the Anope Team.
+ * Based on the original code of Epona by Lara.
+ * Based on the original code of Services by Andy Church.
+ *
+ * $Id: shadowircd5.h 1.0 01-24-2009 $
+ */
+
