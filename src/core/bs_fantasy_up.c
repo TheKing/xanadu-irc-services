ee1c2d74f1d6c4f8e51366a1c49ce6e16a8c0cd2
zach <zach@drivenirc.com>
2009-01-26 19:55:37 -0500

    added comment header, again

---
+++ src/core/bs_fantasy_up.c
@@ -0,0 +1,10 @@
+/* BotServ [F] : !up (Core)
+ *
+ * (C) 2009 Xanadu IRC Services Team
+ *
+ * Based on the original code of Anope by the Anope Team.
+ * Based on the original code of Epona by Lara.
+ * Based on the original code of Services by Andy Church.
+ *
+ * $Id: bs_fantasy_up.c 01-26-2009 digitalman $
+ */
